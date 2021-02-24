38e2b260a232211ebef27937cdae8cac978d907e
Mostafa Nassr <mostafa.nassr88@gmail.com>
2021-02-24 12:46:31 -0500

    solution with no tests

--- app.py
+++ app.py
@@ -1,30 +1,66 @@
 from flask import Flask, render_template, session, request, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
-from flask_cors import CORS, cross_origin
 from boggle import Boggle
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = '12345'
-
-CORS(app, resources=r'/*')
-
 debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+guesses = set()
 
 @app.route('/')
 def show_board():
+    """ Sets up and shows the board game """
+
     boggle_board = boggle_game.make_board()
     session["board"] = boggle_board
     return render_template('board.html', board=boggle_board)
 
-def check_valid_word(word):
-    msg = boggle_game.check_valid_word(session['board'], word)
+def check_guess_validity(guess):
+    """ Checks the validity of the guessed word, then returns a msg
+    - if word exists and is on board: msg => "ok" or msg => "word-already-used" (if used before)
+    - if word exists but not on board: msg => "not-on-board"
+    - if word doesnt exist: msg => "not-word"
+    """
+
+    msg = boggle_game.check_valid_word(session['board'], guess)
+    if msg == "ok":
+        if guess in guesses:
+            msg = "word-already-used"
+        else:
+            guesses.add(guess)
+    
     return msg
 
+
 @app.route('/', methods=['POST'])
-@cross_origin(allow_headers=['Content-Type'])
 def submit_guess():
-    guess = request.get_json().get('guess', '')
-    response = {"result": check_valid_word(guess)}
+    """ Receives a guess and checks its validity, then responds with a msg """
+
+    guess = request.json.get('guess', ' ')
+    msg = check_guess_validity(guess)
+    response = { "result": msg }
+    return jsonify(response)
+
+def update_user_stats(score):
+    """ Helper function: updates high_score & played_games after each game """
+
+    played = session.get('played', 0) + 1
+    session['played'] = played
+
+    if session.get('high_score') or session.get('high_score') == 0:
+        if score > session['high_score']:
+            session['high_score'] = score
+    else:
+        session['high_score'] = 0
+
+@app.route('/endgame', methods=['POST'])
+def get_user_stats():
+    """ Receives the user's score at the end of the game, updates high_score & played_games, then sends it back """
+
+    score = request.json.get('score')
+    update_user_stats(score)
+    response = { "high_score": session['high_score'], "played": session['played'] }
     return jsonify(response)
--- static/app.js
+++ static/app.js
@@ -1,21 +1,54 @@
-const apiURL = 'http://localhost:5000';
 const $guessForm = $('#guess-form'),
-    $guess = $('#word');
+    $guess = $('#word'),
+    $score = $('#score'),
+    $timer = $('#timer');
 
-async function submitGuess(evt) {
-    evt.preventDefault();
-    const word = $guess.val();
-    const res = await axios({
-        url: `${apiURL}`,
-        method: "POST",
-        headers: {  
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-        data: { guess: word }
-    });
-    console.log(res)
-    $guessForm.trigger('reset');
+class FlaskBoggle {
+    constructor() {
+        this.baseUrl = 'http://localhost:5000';
+    }
+
+    /* Alerts user of guess validity, updates current score if word is valid, then resets guess form */
+    updateUI(result, word) {
+        alert(`${word}: ${result}`);
+        if (result === 'ok') {
+            $score.text(parseInt($score.text()) + word.length);
+        };
+        $guessForm.trigger('reset');
+    }
+
+    /* Submits the user's guess to the API */
+    async submitGuess(evt) {
+        evt.preventDefault();
+        const word = $guess.val();
+        const res = await axios({
+            url: this.baseUrl,
+            method: "POST",
+            data: { guess: word }
+        });
+        this.updateUI(res.data.result, word);
+    }
+
+    /* Sends the user's round score to the API (at end of game) */
+    async sendUserStats() {
+        const res = await axios({
+            url: `${this.baseUrl}/endgame`,
+            method: "POST",
+            data: { score: parseInt($score.text()) }
+        });
+        console.log(res.data);
+    }
 }
 
-$guessForm.on('submit', submitGuess);
+$(function() {
+    // init function
+    const app = new FlaskBoggle();
+    $guessForm.on('submit', (evt) => app.submitGuess.call(app, evt));
+
+    // After 60 seconds, ends game and sends score to API
+    setTimeout(function() {
+        $guessForm.addClass('hidden');
+        $timer.removeClass('hidden');
+        app.sendUserStats();
+    }, 60000)
+});
